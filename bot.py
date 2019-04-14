ccacc609685a5f124cf7f9e82b7c7196ae39dcf2
Gumilghost <szymon489984@gmail.com>
2019-04-14 16:48:04 +0200

    Poprawka bota

--- bot.py
+++ bot.py
@@ -2,25 +2,27 @@ from crawler import Crawler
 
 
 class Bot:
+    db = 0
+    mod = 0
+
     def __init__(self, db):
         self.db = db
-        link = db.select().where(db.is_done == 0).first()
-        self.url = link.url
-        self.id = link.id
-        self.link = link
-
+        self.mod = db.select().where(db.is_done == 0).first()
 
     def start(self):
         bot = Crawler()
-        bot.set_url(self.url)
+        bot.set_url(self.mod.url)
         links = bot.search()
 
         for link in links:
-            if str(link) == str(self.db.select().where(self.db.url == link)):
-                # break
-                print('niewpisuje' + str(link))
+            count = self.db.select().where(self.db == link).count()
+
+            if count > 0:
+                print('Nie wpisuje ' + str(link))
+                continue
             else:
-                print('wpisuje' + str(link))
-                # self.db.create(url=link, is_done=0)
-        # self.link.is_done = 1
-        # self.link.save()
+                print('Wpisuje ' + str(link))
+                self.db.create(url=link, is_done=0)
+
+        self.mod.is_done = 1
+        self.mod.save()
